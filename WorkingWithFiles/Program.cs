76bf10af13310198573359bac4f31ddfd6c6ffae
powermtest <powermtest@gmail.com>
2018-03-17 13:54:00 +0100

    Idziemy dalej z wykładami

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WorkingWithFiles
 {
@@ -10,6 +11,23 @@ namespace WorkingWithFiles
     {
         static void Main(string[] args)
         {
+            var sciezka = @"c:\temp\moj.plik";
+            File.Copy(sciezka, @"d:\temp\moj.plik", true);
+            File.Delete(sciezka);
+            if (File.Exists(sciezka))
+            {
+                //cos tu mozna zrobic
+            }
+            var zawartosc = File.ReadAllText(sciezka);
+
+            var fileInfo = new FileInfo(sciezka);
+            fileInfo.CopyTo("cokolwiek tutaj");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //cos do zrobienia
+            }
+
         }
     }
 }
