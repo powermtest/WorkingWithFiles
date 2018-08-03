908e5361880ce1410d7583a6df2a8df640a9c3a6
powermtest <powermtest@gmail.com>
2018-08-03 08:09:26 +0200

    idziemy dalej

--- DirectoryAndDirectoryInfo/Program.cs
+++ DirectoryAndDirectoryInfo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DirectoryAndDirectoryInfo
 {
@@ -10,6 +11,15 @@ namespace DirectoryAndDirectoryInfo
     {
         static void Main(string[] args)
         {
+            var path = @"c:\temp\";
+            var sciezka = @"C:\Users\programowanie\source\repos\";
+            Directory.CreateDirectory(path+"1"+"2");
+
+            var files = Directory.GetFiles(sciezka, "*.cs*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
         }
     }
 }
